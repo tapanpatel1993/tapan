6cdc7eca2d3f8bb1eac720311e8db52f885bae1c
tapan <tapanpatel1992@gmail.com>
2020-07-14 10:08:22 +0530

    Updates

---
+++ precache-manifest.dfabad0c9b000482cfb3b2404f521f01.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c0fa30f415b35a0f41411528fe74ffd",
+    "url": "/tapan/index.html"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/tapan/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "140f047418baf7aacced",
+    "url": "/tapan/static/css/main.5bb59656.chunk.css"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/tapan/static/js/2.f690e5d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tapan/static/js/2.f690e5d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "140f047418baf7aacced",
+    "url": "/tapan/static/js/main.5bbceeec.chunk.js"
+  },
+  {
+    "revision": "a0fd5b664edf5c3955e8",
+    "url": "/tapan/static/js/runtime-main.b4e354da.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/tapan/static/media/SEGA.12a3db8b.woff"
+  }
+]);
